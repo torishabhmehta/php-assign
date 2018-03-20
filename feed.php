879e785e121a2d6ff10021c90bd1de611cde152d
harshit <you@example.com>
2018-03-21 00:14:39 +0530

    nearly complete

---
+++ feed.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+//connection variables
+$db_host="192.168.121.187";
+$db_user="first_year";
+$db_pass="first_year";
+$db="first_year_db";
+
+//mysqli connection
+$conn = new mysqli($db_host, $db_user, $db_pass,$db);
+
+//check connection
+if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+        <title>common feed</title>
+        <style>
+        p{
+        background-color:cyan;}
+        body{
+        background-color:magenta;}
+        </style>
+
+</head>
+<body>
+        <form action="post.php" method="post">
+<textarea name='text'></textarea>
+<input type="submit" name="submit">
+</form>
+
+<?php
+
+//connection variables
+$db_host="192.168.121.187";
+$db_user="first_year";
+$db_pass="first_year";
+$db="first_year_db";
+
+//mysqli connection
+$conn = new mysqli($db_host, $db_user, $db_pass,$db);
+
+//check connection
+if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+}
+
+$sql1 = "SELECT * FROM rishabh_feed;";
+$result = $conn->query($sql1);
+if ($result->num_rows > 0) {
+
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+            echo "<p> " . $row["feed"]. "<br>Commented by: ".$row["username"]." at ".$row["time"]." </p> ";
+      }
+  } else {
+      echo "0 results";
+  }
+
+  ?>
+
+</body>
+</html>
+
+
--- profile.php
+++ profile.php
@@ -8,22 +8,31 @@ ini_set('display_errors', 'On');
 <html>
 <head>
   <title>complete-profile</title>
-      <link rel="stylesheet" type="text/css" href="profile.css">
-          </head>
-              <body>
-                  <div id="a">
-                        <div id="cover">
-                                 <?php  echo '<img src="../images/'.$_SESSION['pp'].'">'; ?>
-                                           <div id="profile">
-                                                         </div>
-                                                                         </div>
-                                                                                         </div>
-                                                                                                         <div id="inform">
-                                                                                                                         <p><span>name</span><span><?php echo $_SESSION["name"]; ?></span></p>
-                                                                                                                                         <p><span>number</span<span><?php echo $_SESSION["phno"]; ?></span></p>
-                                                                                                                                                         <p><span>email</span><span><?php echo $_SESSION["email"]; ?></span></p>
-                                                                                                                                                                        
-                                                                                                                                                                         </div>
-                                                                                                                                                                                         </body>
-                                                                                                                                                                                                         </html>
+ <link rel="stylesheet" type="text/css" href="profile.css">
+</head>
+
+<body>
+  <div id="a">
+    <div id="cover">
+        <?php  echo '<img src="../images1/'.$_SESSION['cp'].'" height="100%" width="100%">'; ?>
+        <div id="profile">
+          <?php  echo '<img src="../images/'.$_SESSION['pp'].'" height="100%" width="100%">'; ?>
+        </div>
+    </div>
+  </div>   
+  <div id="inform">
+    <p><span>name</span><span><?php echo $_SESSION["name"]; ?></span></p>      
+    <p><span>number</span<span><?php echo $_SESSION["phno"]; ?></span> </p>    
+    <p><span>email</span><span><?php echo $_SESSION["email"]; ?></span></p>
+  </div>
+  <div>
+    <button id='a' onclick="feed();">Go To Feed Page</button>
+  </div>
+  <script type="text/javascript">
+    function feed()
+    {window.location="feed.php";}
+  </script>
+</body>
+</html>
+
 
